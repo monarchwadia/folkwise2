0376ae1aebd80a25da5fe4c32222db1de44d9189
Monarch Wadia <monarchwadia@gmail.com>
2022-10-26 04:09:04 -0400

    Markdown link working.

--- src/routes/journal/[slug]/+page.svelte
+++ src/routes/journal/[slug]/+page.svelte
@@ -1,18 +1,32 @@
 <script lang="ts">
 	import { markdowns } from './../markdowns';
 	import { page } from '$app/stores';
+	import { browser, dev, prerendering } from '$app/environment';
+	import { onMount } from 'svelte';
 
+	let ref: any;
+
+	// find the markdown
 	const thisMarkdown = markdowns.find((m) => {
-		console.log(m.slug, $page.params.slug);
 		return m.slug === $page.params.slug;
 	});
 
+	// get the render function
 	const thisMarkdownRender = thisMarkdown && thisMarkdown.render;
-	console.log(thisMarkdownRender.render);
+
+	// browser-only behaviour
+	onMount(() => {
+		if (thisMarkdownRender && browser) {
+			new thisMarkdownRender({ target: ref });
+		}
+	});
 </script>
 
 <div>
-	{#if thisMarkdownRender}
-		{@html thisMarkdownRender.render && thisMarkdownRender.render().html}
-	{/if}
+	<div id="out" bind:this={ref}>
+		<!-- This is backend mode... don't do it in the browser. -->
+		{#if thisMarkdownRender && !browser}
+			{@html thisMarkdownRender.render && thisMarkdownRender.render().html}
+		{/if}
+	</div>
 </div>
