b4326ef8e529379e7834aecd47a0f5049236301f
Monarch Wadia <monarchwadia@gmail.com>
2022-11-06 18:40:20 -0500

    More fixes to markdown

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,1 @@
-<script>
-	import { markdowns } from './journal/markdowns';
-</script>
-
-<div>
-	{#each markdowns as Markdown}
-		<Markdown.render />
-	{/each}
-</div>
+<div>Todo</div>
--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,11 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
-import { mdsvex } from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    extensions: ['.svelte', '.md', '.svx'],
+    extensions: ['.svelte'],
     preprocess: [
-        preprocess(),
-        mdsvex({
-            extensions: ['.md', '.svx'],
-            layout: 'src/routes/journal/_post.svelte'
-        })
+        preprocess()
     ],
     kit: {
         adapter: adapter(),
