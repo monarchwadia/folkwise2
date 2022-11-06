b384e0b17f1ad6e483af68364b305faa2db57ce8
Monarch Wadia <monarchwadia@gmail.com>
2022-11-06 18:46:52 -0500

    Added viteStaticCopy plugin.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { viteStaticCopy } from "vite-plugin-static-copy"
 import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		viteStaticCopy({
+			targets: [
+				{ src: 'journal', dest: 'journal' },
+				{ src: 'patterns', dest: 'patterns' },
+			]
+		})
+	]
 };
 
 export default config;
