8620f09dadd3683317f342dacc605e752277884f
Monarch Wadia <monarchwadia@gmail.com>
2022-11-09 17:34:22 -0500

    Salescard and stickiness.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script>
 	import HeaderMetatags from 'src/components/HeaderMetatags.svelte';
 	import '../app.css';
+	import SalesCard from './SalesCard.svelte';
 
 	const links = [
 		{ href: '/', label: 'Journal' },
@@ -11,68 +12,61 @@
 </script>
 
 <!-- Body -->
-<div class="mx-auto max-w-xl flex flex-col gap-8 p-8">
-	<HeaderMetatags />
-	<!-- Navbar -->
-	<div class="navbar bg-base-100 border-2 rounded-md border-base-content">
-		<div class="flex-1">
-			<a href="/" class="btn btn-ghost normal-case text-xl">Folkwise.io</a>
-		</div>
-		<div class="flex-none hidden sm:block">
-			<ul class="menu menu-horizontal p-0">
-				{#each links as link}
-					<li>
-						<a href={link.href}>{link.label}</a>
-					</li>
-				{/each}
-			</ul>
-		</div>
-		<div class="flex-none sm:hidden dropdown">
-			<label tabindex="0" class="btn btn-ghost btn-circle">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					class="h-5 w-5"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke="currentColor"
-					><path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="2"
-						d="M4 6h16M4 12h16M4 18h7"
-					/></svg
+<div class="flex flex-row gap-8 p-8 justify-center">
+	<div class="flex flex-col gap-8 w-full max-w-xl">
+		<HeaderMetatags />
+		<!-- Navbar -->
+		<div
+			style="box-shadow: 0px -40px 30px 60px hsl(var(--b1))"
+			class="navbar bg-base-100 border-2 rounded-md border-base-content sticky top-8 shadow-neutral"
+		>
+			<div class="flex-1">
+				<a href="/" class="btn btn-ghost normal-case text-xl">Folkwise.io</a>
+			</div>
+			<div class="flex-none hidden sm:block">
+				<ul class="menu menu-horizontal p-0">
+					{#each links as link}
+						<li>
+							<a href={link.href}>{link.label}</a>
+						</li>
+					{/each}
+				</ul>
+			</div>
+			<div class="flex-none sm:hidden dropdown">
+				<label tabindex="0" class="btn btn-ghost btn-circle">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="h-5 w-5"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke="currentColor"
+						><path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							d="M4 6h16M4 12h16M4 18h7"
+						/></svg
+					>
+				</label>
+				<ul
+					tabindex="0"
+					class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+					style={'--tw-translate-x: -150px'}
 				>
-			</label>
-			<ul
-				tabindex="0"
-				class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-				style={'--tw-translate-x: -150px'}
-			>
-				{#each links as link}
-					<li>
-						<a href={link.href}>{link.label}</a>
-					</li>
-				{/each}
-			</ul>
+					{#each links as link}
+						<li>
+							<a href={link.href}>{link.label}</a>
+						</li>
+					{/each}
+				</ul>
+			</div>
 		</div>
-	</div>
 
-	<!-- Looking for work -->
-	<div class="card bg-slate-900 text-base-content">
-		<div class="card-body">
-			<h2 class="card-title">💼 I'm looking for work.</h2>
-			<p>
-				I'm looking for 100% remote, hands-on programming work. If you'd like to hire me,
-				<a
-					class="link"
-					href="https://linkedin.com/in/monarchwadia"
-					target="_blank"
-					rel="noopener noreferrer nofollow">let's get in touch</a
-				>.
-			</p>
-		</div>
+		<main>
+			<slot />
+		</main>
 	</div>
-	<main>
-		<slot />
-	</main>
+	<div class="max-w-md sticky top-0">
+		<SalesCard />
+	</div>
 </div>
---
+++ src/routes/SalesCard.svelte
@@ -0,0 +1,34 @@
+<!-- Looking for work -->
+<div class="sticky top-8">
+	<div
+		class="card bg-zinc-900 border-zinc-800 border-2 text-base-content cursor-pointer hover:bg-zinc-900"
+	>
+		<div class="card-body flex flex-col gap-4">
+			<h2 class="card-title flex flex-row gap-4">
+				<span>💼</span><span>Stop burning money on headhunters.</span>
+			</h2>
+			<p>$37,500.</p>
+			<p>
+				That's how much many headhunters charge for every single software engineer they hire for
+				you.
+			</p>
+			<p>That's also the cost of brand new Mercedes Benz.</p>
+			<p>Each. And. Every. Time. 💸💸💸</p>
+			<p>
+				Wouldn't you rather have an in-house pipeline you can rely on this quarter, and for years to
+				come?
+			</p>
+			<p>
+				You're <span class="italic font-bold underline">much</span>
+				closer to an effective, healthy hiring funnel than you think.
+				<a
+					class="link"
+					href="https://linkedin.com/in/monarchwadia"
+					target="_blank"
+					rel="noopener noreferrer nofollow"
+					>Let's talk.
+				</a>
+			</p>
+		</div>
+	</div>
+</div>
