e19e6f71c79763aabc1b73bf8ca7292f67551c0c
Monarch Wadia <monarchwadia@gmail.com>
2022-11-09 17:56:06 -0500

    Update sales card

--- src/routes/SalesCard.svelte
+++ src/routes/SalesCard.svelte
@@ -1,34 +1,31 @@
 <!-- Looking for work -->
 <div class="sticky top-8">
-	<div
-		class="card bg-zinc-900 border-zinc-800 border-2 text-base-content cursor-pointer hover:bg-zinc-900"
-	>
+	<div class="card bg-zinc-900 border-zinc-800 border-2 text-base-content hover:bg-zinc-900">
 		<div class="card-body flex flex-col gap-4">
 			<h2 class="card-title flex flex-row gap-4">
-				<span>💼</span><span>Stop burning money on headhunters.</span>
+				<span>Paying too much for external recruiters?</span>
 			</h2>
-			<p>$37,500.</p>
+			<p class="text-2xl font-bold text-red-500">▼ $37,500</p>
 			<p>
-				That's how much many headhunters charge for every single software engineer they hire for
-				you.
+				That's what most companies are paying headhunters <span class="italic"
+					>for every single software engineer they hire.</span
+				>
 			</p>
-			<p>That's also the cost of brand new Mercedes Benz.</p>
-			<p>Each. And. Every. Time. 💸💸💸</p>
+			<p>That's also the cost of a brand new Mercedes Benz. Each. And. Every. Time. 💸💸💸</p>
 			<p>
-				Wouldn't you rather have an in-house pipeline you can rely on this quarter, and for years to
-				come?
+				But it's possible to bring your cost of talent acquisition down to under <span
+					class="text-green-600 font-bold">&lt;$10,000.</span
+				> And it can be done in weeks, not months.
 			</p>
+			<button
+				class="btn btn-outline border-green-600 text-green-600 font-normal text-lg normal-case hover:btn-success"
+				>Let us show you how.</button
+			>
 			<p>
 				You're <span class="italic font-bold underline">much</span>
-				closer to an effective, healthy hiring funnel than you think.
-				<a
-					class="link"
-					href="https://linkedin.com/in/monarchwadia"
-					target="_blank"
-					rel="noopener noreferrer nofollow"
-					>Let's talk.
-				</a>
+				closer to an effective, healthy, and reliable in-house hiring process than you think.
 			</p>
+			<p class="link text-sm text-zinc-500">This isn't relevant to me, please hide it.</p>
 		</div>
 	</div>
 </div>
