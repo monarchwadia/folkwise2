853fae0bf965f8764c7141822945511f8572be01
Monarch Wadia <monarchwadia@gmail.com>
2022-11-10 16:46:15 -0500

    Got rid of dead pages related to journal

--- journal/2022-09-10-recent-experiences-in-the-job-market.md
+++
@@ -1,6 +0,0 @@
----
-title: 'Recent experiences in the job market'
-date: 2022-09-10
----
-
-Looking for a job is infinitely easier than building a business. But doing business pays a lot more than finding a job.
--- src/routes/journal/journalMarkdownService.ts
+++
@@ -1,9 +0,0 @@
-import { MarkdownService } from 'src/services/MarkdownService';
-import path from "path";
-import * as url from 'url';
-const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
-
-export const journalMarkdownService = new MarkdownService({
-  srcDir: (path.resolve(__dirname, "../../../journal")), // this path is carefully set to correlate to the config in vite.config.ts
-  extensions: ["md"],
-});
