8defab17e5a320934a4782b76d94713dbc8d8ba2
Monarch Wadia <monarchwadia@gmail.com>
2022-11-10 17:37:16 -0500

    Configured prose

--- src/components/Markdown.svelte
+++ src/components/Markdown.svelte
@@ -2,17 +2,35 @@
 	export let title: string;
 	export let url: string | undefined = undefined;
 	export let content: string;
+
+	const proseCss = `prose text-base-content
+		prose-h1:text-2xl
+
+		prose-h2:text-xl
+
+		prose-h3:text-lg 
+			prose-h3:font-bold
+
+		prose-h4:text-lg 
+			prose-h4:font-normal
+
+		prose-h5:text-md
+			prose-h5:font-bold
+		
+		prose-h6:text-md
+			prose-h6:underline
+	`;
 </script>
 
 <div class="flex flex-col gap-4">
-	<h1 class="text-2xl font-bold">
+	<h1 class="text-3xl font-bold">
 		{#if url}
 			<a class="link" href={url}>{title}</a>
 		{:else}
 			{title}
 		{/if}
 	</h1>
-	<div class="prose text-base-content">
+	<div class={proseCss}>
 		{@html content}
 	</div>
 </div>
